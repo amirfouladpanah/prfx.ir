1f3e9b90a49c9c888c250660b18c51d5d035dabc
Amir Fouladpanah <47280601+amirfouladpanah@users.noreply.github.com>
2026-09-06 17:09:27 +0330

    feat: add product variant model

---
+++ backend/app/Models/ProductVariant.php
@@ -0,0 +1,4 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class ProductVariant extends Model { public $timestamps=false; protected $guarded=[]; public function product(){return $this->belongsTo(Product::class);} }
