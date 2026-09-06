c307283f6a79a440e5d98a0da3b84f0b75096120
Amir Fouladpanah <47280601+amirfouladpanah@users.noreply.github.com>
2026-09-06 17:10:08 +0330

    feat: add product image model

---
+++ backend/app/Models/ProductImage.php
@@ -0,0 +1,4 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class ProductImage extends Model { public $timestamps=false; protected $guarded=[]; public function product(){return $this->belongsTo(Product::class);} }
